2426a4c78d2569e286e781cc5001a20dbb6492e1
chinmayakumardas <chinmayakumardas2000@gmail.com>
2025-07-16 00:12:57 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/portfolio',
   assetPrefix: '/portfolio',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
